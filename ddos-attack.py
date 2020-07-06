043b97ac2321038c0f470d63403d07fb027cf9ed
taahro <35579185+taahro@users.noreply.github.com>
2020-07-05 21:48:20 -0400

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -18,12 +18,7 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
-print
-print "Author   : Tech Tushar"
-print "You Tube : https://www.youtube.com/c/TechTushar"
-print "github   : https://github.com/Techtushar07"
-print "Facebook : https://www.facebook.com/daveratushar/"
+os.system("figlet DDos Attack"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
